513127cbbf3f7a5b2e676da42dbac98d72e522de
NatalieCastien <natalie.castien@gmail.com>
2020-06-23 16:32:12 +0200

    new firebase

--- api-client.js
+++ api-client.js
@@ -1,36 +1,37 @@
-const baseUrl = "https://wincacademydatabase.firebaseio.com"
+const baseUrl = "https://to-do-25688.firebaseio.com";
 const firstName = "Natalie";
 
 const getAllTasks = async () => {
-    const apiUrl = `${baseUrl}/${firstName}/tasks.json`;
-    try {
-        const result = await fetch(apiUrl, {method: 'GET'});    
-        const jsondata = await result.json();    
-        return jsondata;
-    } catch (error) {console.log(error)}
-}
+  const apiUrl = `${baseUrl}/tasks.json`;
+  try {
+    const result = await fetch(apiUrl, { method: "GET" });
+    const jsondata = await result.json();
+    return jsondata;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const postNewTask = async (data = {}) => {
-    const apiUrl = `${baseUrl}/${firstName}/tasks.json`;
-    const response = await fetch(apiUrl, {
-      method: 'POST', 
-      body: JSON.stringify(data)
-    });
-    return response.json();
-  };
+  const apiUrl = `${baseUrl}/tasks.json`;
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
 
-  const updateTask = async (hashId, data = {}) => {
-    const apiUrl = `${baseUrl}/${firstName}/tasks/${hashId}.json`;
-    const response = await fetch(apiUrl, {
-      method: 'PUT', 
-      body: JSON.stringify(data)
-    });
-    return response.json();
-  };
+const updateTask = async (hashId, data = {}) => {
+  const apiUrl = `${baseUrl}/tasks/${hashId}.json`;
+  const response = await fetch(apiUrl, {
+    method: "PUT",
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
 
-  const deleteTask = async (hashId) => {
-    const apiUrl = `${baseUrl}/${firstName}/tasks/${hashId}.json`;
-    const response = await fetch(apiUrl, {method: 'DELETE'});
-    return response.json();
-  }
-  
+const deleteTask = async (hashId) => {
+  const apiUrl = `${baseUrl}/tasks/${hashId}.json`;
+  const response = await fetch(apiUrl, { method: "DELETE" });
+  return response.json();
+};
